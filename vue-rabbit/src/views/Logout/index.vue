61e85f9c29fd401a4f108e2993a2a1deb0dc1033
神行者 <m18222349531_1@163.com>
2024-11-23 13:37:43 +0800

    加入了icon图标库
    加入了icon图标库

--- vue-rabbit/src/views/Logout/index.vue
+++ vue-rabbit/src/views/Logout/index.vue
@@ -1,4 +1,14 @@
+<script setup>
+import LayoutNav from "@/views/Logout/components/LayoutNav.vue";
+import LayoutHeader from "@/views/Logout/components/LayoutHeader.vue";
+import LayoutFooter from "@/views/Logout/components/LayoutFooter.vue";
+</script>
+
 <template>
-  <div>首页</div>
+  <LayoutNav />
+  <LayoutHeader />
   <RouterView />
+  <LayoutFooter />
 </template>
+
+<style scoped></style>
