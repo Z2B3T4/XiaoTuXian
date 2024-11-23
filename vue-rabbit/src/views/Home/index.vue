94e5c2e85834f8d9e4d345502f12bd0387a9a613
神行者 <m18222349531_1@163.com>
2024-11-23 17:54:03 +0800

    加上了人气推荐组件插槽
    加上了人气推荐组件插槽

--- vue-rabbit/src/views/Home/index.vue
+++ vue-rabbit/src/views/Home/index.vue
@@ -6,6 +6,10 @@
   <HomeNew />
   <HomeHot />
   <homeProduct />
+  <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+    <div>我是新鲜好物的插槽</div>
+  </HomePanel>
+  <HomePanel title="人气推荐" sub-title="人气推荐 好多商品"> </HomePanel>
 </template>
 <script setup>
 import HomeBanner from "@/views/Home/components/HomeBanner.vue";
@@ -13,5 +17,6 @@ import HomeCategory from "@/views/Home/components/HomeCategory.vue";
 import HomeHot from "@/views/Home/components/HomeHot.vue";
 import HomeNew from "@/views/Home/components/HomeNew.vue";
 import HomeProduct from "@/views/Home/components/HomeProduct.vue";
+import HomePanel from "@/views/Home/components/HomePanel.vue";
 </script>
 <style></style>
