f9ba45998ff3c6bbb5b494aa90cde4333635c6ac
神行者 <m18222349531_1@163.com>
2024-11-23 13:08:27 +0800

    进行了二级路由
    进行了二级路由

--- vue-rabbit/src/router/index.ts
+++ vue-rabbit/src/router/index.ts
@@ -1,23 +1,37 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// createRouter 创建路由对象
+// createWebHistory 创建history模式的路由
+
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "@/views/Login/index.vue";
+import Logout from "@/views/Logout/index.vue";
+import Home from "@/views/Home/index.vue";
+import Category from "@/views/Categoty/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: "/",
+      name: "logout",
+      component: Logout,
+      children: [
+        {
+          path: "", // 这里进行置空就是表示默认加载/ 的时候就把他加载上
+          name: "home",
+          component: Home,
+        },
+        {
+          path: "category",
+          component: Category,
+        },
+      ],
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: "/login",
+      name: "login",
+      component: Login,
     },
   ],
-})
+});
 
-export default router
+export default router;
