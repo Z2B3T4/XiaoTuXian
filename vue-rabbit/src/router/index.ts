0d092db148673440d0dd32c68f54926ad05caf52
神行者 <m18222349531_1@163.com>
2024-11-24 09:57:27 +0800

    添加了二级路由跳转
    添加了二级路由跳转

--- vue-rabbit/src/router/index.ts
+++ vue-rabbit/src/router/index.ts
@@ -6,6 +6,7 @@ import Login from "@/views/Login/index.vue";
 import Logout from "@/views/Logout/index.vue";
 import Home from "@/views/Home/index.vue";
 import Category from "@/views/Categoty/index.vue";
+import SubCategory from "@/views/SubCategory/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +24,11 @@ const router = createRouter({
         {
           path: "category/:id",
           component: Category,
+        },
+        {
+          path: "category/sub/:id",
+          name: "subcategory",
+          component: SubCategory,
         },
       ],
     },
