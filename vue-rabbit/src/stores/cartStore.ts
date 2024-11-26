46bfe4cae0e7886d604cea2e29571b090aaf3270
神行者 <m18222349531_1@163.com>
2024-11-26 09:31:37 +0800

    合并购物车的操作
    合并购物车的操作

--- vue-rabbit/src/stores/cartStore.ts
+++ vue-rabbit/src/stores/cartStore.ts
@@ -1,25 +1,54 @@
 import { defineStore } from "pinia";
 import { computed, ref } from "vue";
+import { useUserStore } from "@/stores/user";
+import { insertCartAPI, findNewCartListAPI, delCartAPI } from "@/apis/cart";
 
 export const useCartStore = defineStore(
   "cart",
   () => {
+    const userStore = useUserStore();
     // 定义state，购物车列表
     const cartList = ref([]);
+    const isLogin = computed(() => userStore.userInfo.token);
+    // 将更新购物车列表的逻辑抽离出来
+    const updateCartList = async () => {
+      const res = await findNewCartListAPI();
+      cartList.value = res.result;
+    };
     // 定义action，addCart
-    const addCart = (goods: any) => {
-      // 这里要进行的逻辑就是，根据传过来的skuId，看看是否可以在cartList找到
-      // 如果能找到就不添加，如果找不到就添加
-      const item = cartList.value.find((item) => item.skuId === goods.skuId);
-      if (item) {
-        item.count++;
+    const addCart = async (goods: any) => {
+      const { skuId, count } = goods;
+      // 这里我们要检验用户是否已经登录，用户登录了就要走登录的逻辑
+      // 这里使用计算属性是因为，计算属性可以返回是否存在，也就是一个bool
+      //   注意这里一定要加上isLogin.value,不然就相当于没加上
+      if (isLogin.value) {
+        // 登陆之后加入购物车逻辑
+        // 这个不是加载到前端，这个是和后端进行通信，让后端去加
+        await insertCartAPI({ skuId, count });
+        updateCartList();
       } else {
-        cartList.value.push(goods);
+        // 这里要进行的逻辑就是，根据传过来的skuId，看看是否可以在cartList找到
+        // 如果能找到就不添加，如果找不到就添加
+        const item = cartList.value.find((item) => item.skuId === goods.skuId);
+        if (item) {
+          item.count++;
+        } else {
+          cartList.value.push(goods);
+        }
       }
     };
-    const delCart = (skuId) => {
-      const index = cartList.value.findIndex((item) => skuId == item.skuId);
-      cartList.value.splice(index, 1);
+    const delCart = async (skuId) => {
+      if (isLogin.value) {
+        await delCartAPI([skuId]);
+        updateCartList();
+      } else {
+        const index = cartList.value.findIndex((item) => skuId == item.skuId);
+        cartList.value.splice(index, 1);
+      }
+    };
+    // 这里进行清空购物车列表
+    const clearCart = () => {
+      cartList.value = [];
     };
     const allCount = computed(() =>
       cartList.value.reduce((a, c) => a + c.count, 0)
@@ -62,6 +91,8 @@ export const useCartStore = defineStore(
       singleCheck,
       addCart,
       delCart,
+      clearCart,
+      updateCartList,
     };
   },
   {
--- vue-rabbit/src/stores/user.ts
+++ vue-rabbit/src/stores/user.ts
@@ -3,19 +3,36 @@ import { ElMessage } from "element-plus";
 import "element-plus/theme-chalk/el-message.css";
 import { defineStore } from "pinia";
 import { ref } from "vue";
-
+import { useCartStore } from "@/stores/cartStore";
+import { mergeCartAPI } from "@/apis/cart";
 export const useUserStore = defineStore(
   "user",
   () => {
+    const cartStore = useCartStore();
     // 定义管理用户数据的对象
     const userInfo = ref({});
     // 定义获取接口的函数api
     const getUserInfo = async ({ account, password }) => {
       const res = await LoginAPI({ account, password });
       userInfo.value = res.result;
+      // 这里用户在登录的时候要把它没有登录时候的购物车合并过来
+      // 这里使用了一个map方法，将原数组映射部分字段组成一个新的数组
+      await mergeCartAPI(
+        cartStore.cartList.map((item) => {
+          return {
+            skuId: item.skuId,
+            selected: item.selected,
+            count: item.count,
+          };
+        })
+      );
+      // 这里就不用写await了，因为updateCartList抽离的时候本身已经写了异步的
+      cartStore.updateCartList();
     };
+    // 这里是用户退出登录，顺便也要清空购物车信息
     const clearUserInfo = () => {
       userInfo.value = {};
+      cartStore.clearCart();
     };
     // 返回信息
     return {
